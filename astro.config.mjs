901429fcbb1ce4b9d1e7bf8277e4a92ae68e5802
Grant Birkinbine <grantbirki@github.com>
2023-03-21 23:17:48 +0000

    update link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grantbirki.github.io',
-  base: '/astro-portfolio',
+  site: 'https://astro-portfolio.birki.io',
+  base: '/',
   trailingSlash: 'never',
   output: 'static',
 });
