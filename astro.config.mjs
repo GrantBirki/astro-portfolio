227e16edcc712c067529e45cad6c97083047463a
grantbirki <grantbirki@github.com>
2023-03-22 10:17:25 +0000

    general updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +10,22 @@ export default defineConfig({
   base: '/',
   trailingSlash: 'never',
   output: 'static',
+
+  integrations: [
+    sitemap(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    mdx(),
+    compress({
+      css: true,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+      logger: 1,
+    }),
+  ],
 });
